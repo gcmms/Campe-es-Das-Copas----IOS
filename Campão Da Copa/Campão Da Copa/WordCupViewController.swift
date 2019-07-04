5792cdf431d88240e4cf9a5e488b820a466dcba3
MacBookFcamara <32177705+gcmms@users.noreply.github.com>
2019-07-04 14:04:10 -0300

    Separação das pastas e criação de algumas das Class

---
+++ Campão Da Copa/Campão Da Copa/WordCupViewController.swift
@@ -0,0 +1,30 @@
+ //
+//  WordCupViewController.swift
+//  Campão Da Copa
+//
+//  Created by Gabriel Chirico Mahtuk de Melo Sanzone on 04/07/19.
+//  Copyright © 2019 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import UIKit
+
+class WordCupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
